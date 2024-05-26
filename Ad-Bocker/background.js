d9093192beddfb049cdfea0bb2dbf3113bb54af0
AqsaSyed01 <157914919+AqsaSyed01@users.noreply.github.com>
2024-05-26 11:51:09 +0500

    Add files via upload

---
+++ Ad-Bocker/background.js
@@ -0,0 +1,24 @@
+const defaultFilters = [
+  "*://*.doubleclick.net/*",
+  "*://partner.googleadservices.com/*",
+  "*://*.googlesyndication.com/*",
+  "*://*.google-analytics.com/*",
+  "*://creative.ak.fbcdn.net/*",
+  "*://*.adbrite.com/*",
+  "*://*.exponential.com/*",
+  "*://*.quantserve.com/*",
+  "*://*.scorecardresearch.com/*",
+  "*://*.urdu.geo.tv//*",
+  "*://*.googleadservices.com/*",
+  "*://*.googletagmanager.com/*",
+  "*://*.googleadservices.com/*",
+  "*://*.googleads.g.doubleclick.net/*",
+];
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    return { cancel: true };
+  },
+  { urls: defaultFilters },
+  ["blocking"]
+);
